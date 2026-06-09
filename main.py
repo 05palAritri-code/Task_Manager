5db76252d40dac0950cb1cf15362dfa399b36bd7
Aritri Pal <05palneha@gmail.com>
2026-06-10 02:42:23 +0530

    add register

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from utils.db import Base, engine
 from task_route import task_routes
 from user_routes import user_routes
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(engine)
 
@@ -10,3 +12,11 @@ app = FastAPI(title="Task Manager", )
 app.include_router(task_routes)
 
 app.include_router(user_routes)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
