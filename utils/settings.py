8c562154fb5439abdee92956fe5a97f3d99a5898
Aritri Pal <05palneha@gmail.com>
2026-06-11 10:58:11 +0530

    add main route

--- utils/settings.py
+++ utils/settings.py
@@ -1,27 +1,27 @@
-# from pydantic_settings import BaseSettings, SettingsConfigDict
-
-# class Settings(BaseSettings):
-#     DB_CONNECTION : str
-#     SECRET_KEY : str
-#     ALGORITHM : str
-#     EXP_TIME: int
-
-#     model_config = SettingsConfigDict(env_file=".env" , extra="ignore")
-
-# settings = Settings()
-
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    DB_CONNECTION: str | None = None
-    SECRET_KEY: str | None = None
-    ALGORITHM: str = "HS256"
-    EXP_TIME: int = 3600
+    DB_CONNECTION : str
+    SECRET_KEY : str
+    ALGORITHM : str
+    EXP_TIME: int
 
-    def validate(self):
-        if not self.DB_CONNECTION or not self.SECRET_KEY:
-            raise ValueError("Missing required environment variables")
+    model_config = SettingsConfigDict(env_file=".env" , extra="ignore")
 
 settings = Settings()
-settings.validate()
+
+# from pydantic_settings import BaseSettings
+
+# class Settings(BaseSettings):
+#     DB_CONNECTION: str | None = None
+#     SECRET_KEY: str | None = None
+#     ALGORITHM: str = "HS256"
+#     EXP_TIME: int = 3600
+
+#     def validate(self):
+#         if not self.DB_CONNECTION or not self.SECRET_KEY:
+#             raise ValueError("Missing required environment variables")
+
+# settings = Settings()
+# settings.validate()
 
